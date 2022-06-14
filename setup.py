12ba3acab5feae4d105a81f889643c4913333fe6
SanniM3 <sannimardhiyah@gmail.com>
2022-06-14 13:15:06 +0100

    changed login format

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.6' 
+VERSION = '0.1.7' 
 DESCRIPTION = 'Rectvision package'
 LONG_DESCRIPTION = 'This package allows the user to interact with the rectvision platform via code'
 
