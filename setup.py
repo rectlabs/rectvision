386f7142396cdfcf9378ef42aba0d2bb4d69cc83
SanniM3 <sannimardhiyah@gmail.com>
2022-06-14 13:06:20 +0100

    changed class imports

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.5' 
+VERSION = '0.1.6' 
 DESCRIPTION = 'Rectvision package'
 LONG_DESCRIPTION = 'This package allows the user to interact with the rectvision platform via code'
 
