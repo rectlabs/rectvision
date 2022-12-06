584e596fecc2ef8fefa752025002ff11dc2240d9
elishatofunmi <elishatofunmi@gmail.com>
2022-12-06 17:49:06 +0100

    main

--- src/testsrc.py
+++
@@ -1,5 +0,0 @@
-from rectvision import data, models, processors, settings
-from rectvision.data import converters
-
-converters.LabelmeToTfCsv(ann_dir='/valid', out_csv_dir='/cocofiles')
-converters.RectvisionConverter(0.6, 0.2, 0.2, 'yolo-txt')
