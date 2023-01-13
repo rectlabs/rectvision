6eb6c2a99d58bb523a2fb05d14659f528baeadf6
elishatofunmi <elishatofunmi@gmail.com>
2023-01-13 23:17:04 +0100

    upgrade setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 from setuptools import setup, find_packages
 import codecs
-import os
+import os, sys, subprocess
+
+# install torch with gpu
+os.system("pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html)
+
+# install detectron2
+detectron2 = "python -m pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu111/torch1.9/index.html"
+os.system(detectron2)
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -10,6 +17,13 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
 VERSION = '3.0.31'
 DESCRIPTION = 'A low-code tool to help create your own AI'
 LONG_DESCRIPTION = 'A package that allows to build your own computer visions and NLP systems'
+
+
+requirement_path = here + '/requirements.txt'
+install_requires = [] 
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
 
 # Setting up
 setup(
@@ -21,8 +35,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=["requests", "importlib-metadata", "boto3==1.22.9", "dtw==1.4.0", "labelme==5.0.1","moviepy==1.0.3", "numpy==1.20.0",
-    "pyJWT==2.4.0", "python-dotenv==0.19.2", "PyYAML==5.4.1", "opencv-contrib-python==4.6.0.66", "sklearn"],
+    install_requires=install_requires,
     keywords=['python', 'NLP', 'computer vision', 'deep learning', 'AI'],
     classifiers=[
         "Development Status :: 1 - Planning",
