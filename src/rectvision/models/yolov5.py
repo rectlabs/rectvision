f52637a9f37fbb627babc8cf01b2d8b470766d58
elishatofunmi <elishatofunmi@gmail.com>
2023-08-16 20:49:12 +0100

    v3.0.69

--- src/rectvision/models/yolov5.py
+++ src/rectvision/models/yolov5.py
@@ -4,6 +4,18 @@ import sys
 import yaml
 import shutil
 
+def otp(length = 5) :
+    # Declare a digits variable 
+    # which stores all digits
+    digits = "0123456789"
+    OTP = ""
+   # length of password can be changed
+   # by changing value in range
+    for i in range(length) :
+        OTP += digits[math.floor(random.random() * 10)]
+ 
+    return OTP
+
 class Yolov5():
     def __init__(self, num_classes, img_size, batch_size, num_epochs, labels, project_name, project_dir):
         self.num_classes = num_classes
@@ -36,11 +48,11 @@ class Yolov5():
 
     def git_clone(self, url):
         process = subprocess.run(["git", "clone", url], capture_output=True, text=True)
-        if process.returncode == 0:
-          print('success!!!')
-        else:
-          print('Something went wrong')
-          print(process.stderr)
+        # if process.returncode == 0:
+        #   print('success!!!')
+        # else:
+        #   print('Something went wrong')
+        #   print(process.stderr)
     
     def create_data_config(self):
         train_path = os.path.join(self.project_dir, "dataset/images/train")
@@ -120,22 +132,25 @@ class Yolov5():
           print(process.stderr)
         
 
-    def inference(self, images, confidence, out_dir):
+    def inference(self, images, out_dir, confidence= 0.3, iou_thres = 0.3):
+      self.outdir =out_dir + '/' + otp()
       self.detect_model = os.path.join(self.project_dir, "yolov5/detect.py")
       self.model_weights = os.path.join(self.project_dir, "yolov5/runs/train", self.project_name, "weights/best.pt")
       print('Inference in progress...')
       process = subprocess.run(["python", self.detect_model, 
                         "--source", images,
+                        "--img",  416,
                         "--weights", self.model_weights,
                         "--conf", str(confidence),
+                        "--iou-thres", str(iou_thres),
                         "--name", self.project_name + '_detections',
                         "--save-txt"], capture_output=True, text=True)
-    #   !python {self.detect_model} --source {images} --weights {self.model_weights} --conf {confidence} --name {self.project_name + '_detections'}
       if process.returncode == 0:
           print('Inference completed successfully!') 
-          print(process.stdout)         
-          shutil.copytree(os.path.join(self.project_dir, "yolov5/runs/detect", self.project_name+'_detections'), out_dir)
-          print('Check {} and {} for detections and annotations generated'.format(os.path.join(self.project_dir, "yolov5/runs/train", self.project_name+'_detections'), out_dir))
+          print(process.stdout)   
+          shutil.copytree(os.path.join(self.project_dir, "yolov5/runs/detect", self.project_name+'_detections'), self.out_dir)
+          print('Check {} and {} for detections and annotations generated'.format(os.path.join(self.project_dir, "yolov5/runs/train", self.project_name+'_detections'), self.out_dir))
+
       else:
           print('Inference could not be completed. Check error below for more details')
           print(process.stderr)
